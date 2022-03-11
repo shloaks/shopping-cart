74349629113ffb64702b61a1abc2344176f0221f
shloaks <85568051+shloaks@users.noreply.github.com>
2022-03-11 20:06:30 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,7 +2,8 @@ import React from "react";
 import Product from "./Product";
 
 export default function Main(props) {
-  const { products, onAdd } = props;
+  const { products, onAdd, prev, next, index, setIndex } = props;
+  console.log(prev, next, index);
   return (
     <main className="block col-2">
       <h2>Products</h2>
@@ -10,6 +11,10 @@ export default function Main(props) {
         {products.map((product) => (
           <Product key={product.id} product={product} onAdd={onAdd}></Product>
         ))}
+      </div>
+      <div style={{ width: "10%", margin: "auto" }}>
+        {prev ? <button onClick={() => setIndex(index - 1)}>Prev</button> : ""}
+        {next ? <button onClick={() => setIndex(index + 1)}>next</button> : ""}
       </div>
     </main>
   );
